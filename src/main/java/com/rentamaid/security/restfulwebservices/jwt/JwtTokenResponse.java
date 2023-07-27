9c865ad82f6c6db67ffab2a6f78d81e7e29deeb5
Efren <alex_007xd@hotmail.com>
2023-07-27 03:36:24 -0600

    Submitting Spring baseline with JWT Authentication to build upon from backend side

---
+++ src/main/java/com/rentamaid/security/restfulwebservices/jwt/JwtTokenResponse.java
@@ -0,0 +1,5 @@
+package com.rentamaid.security.restfulwebservices.jwt;
+
+public record JwtTokenResponse(String token) {}
+
+
---
+++ src/main/resources/data.sql
@@ -0,0 +1,21 @@
+CREATE TABLE IF NOT EXISTS users (
+    id INT PRIMARY KEY,
+    email VARCHAR(255),
+    password VARCHAR(255),
+    firstname VARCHAR(255),
+    lastname VARCHAR(255),
+    userrole VARCHAR(255)
+);
+
+INSERT INTO users (id, email, password, firstname, lastname, userrole)
+VALUES (1, 'admin@mail.com', '12345678', 'Efren', 'Ruiz', 'admin');
+
+INSERT INTO users (id, email, password, firstname, lastname, userrole)
+VALUES (2, 'cliente@mail.com', '12345678', 'Oscar', 'Cortes', 'client');
+
+INSERT INTO users (id, email, password, firstname, lastname, userrole)
+VALUES (3, 'cleaner@mail.com', '12345678', 'Dulce', 'De Nuez', 'cleaner');
+
+INSERT INTO users (id, email, password, firstname, lastname, userrole)
+VALUES (4, 'admin2@mail.com', '12345678', 'Jose', 'Villafranca', 'admin');
+
